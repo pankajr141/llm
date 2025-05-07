87eedcf8ffd4737023c6694bc34fb814af5e3679
Pankaj Rawat <pankajr141@gmail.com>
2025-05-07 06:50:39 +0000

    package release

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
+import os
+print(os.getcwd())
 with open("requirements.txt", "r") as f:
+#with open("requires.txt", "r") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='llm_bhasha',
+    name='llm_bhasa',
     version='0.1',
     packages=find_packages(),
     install_requires=requirements,
